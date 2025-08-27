57292efe76e5c6eed46801c8c0456d7926151bcd
Eduardo Rittner <eduardorittnerc@gmail.com>
2025-08-27 13:28:59 -0300

    formatting: Enable auto-format for everything except java

--- lua/custom/conform.lua
+++ lua/custom/conform.lua
@@ -3,16 +3,24 @@ return {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = true,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'black' },
         c = { 'clang-format' },
         rust = { 'rust_analyzer' },
       },
+
+      -- Enables format on save for everything *except* filetypes declared in disable_filetypes
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        local disable_filetypes = { java = false }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
     },
   },
 }
