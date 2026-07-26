313d7b855835c8cbb4f20b15226fbe694ff6a0b4
eduardorittner <eduardorittnerc@gmail.com>
2026-07-26 14:42:55 -0300

    fix <space>k for rust code

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -6,8 +6,29 @@
 --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 
 local setup = function()
+  -- Fix for Neovim 0.12 query directive node capture wrapping in nvim-treesitter
+  local ts_query = require('vim.treesitter.query')
+  local opts = { force = true, all = false }
+
+  ts_query.add_directive('set-lang-from-info-string!', function(match, _, bufnr, pred, metadata)
+    local capture_id = pred[2]
+    local node = match[capture_id]
+    if type(node) == 'table' and not node.range then
+      node = node[1]
+    end
+    if not node then
+      return
+    end
+    local text = vim.treesitter.get_node_text(node, bufnr)
+    if text then
+      local injection_alias = text:lower()
+      local match_ft = vim.filetype.match { filename = 'a.' .. injection_alias }
+      metadata['injection.language'] = match_ft or injection_alias
+    end
+  end, opts)
+
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'bash', 'c', 'lua', 'rust' },
+    ensure_installed = { 'bash', 'c', 'lua', 'rust', 'markdown', 'markdown_inline' },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
